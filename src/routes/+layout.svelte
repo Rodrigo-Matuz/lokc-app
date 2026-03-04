d7982ada4bfa28470bb9b7eedeed7e807eca1f83
Rodrigo-Matuz <rodrigosantoslima1311@gmail.com>
2026-03-04 15:42:31 -0300

    Add tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import "../layout.css";
+    // import { ModeWatcher } from "mode-watcher";
+    // biome-ignore lint/correctness/noUnusedVariables: For now it's there following standarts for tailwind css
+        const { children } = $props();
+</script>
+
+<!-- <ModeWatcher defaultMode="dark" /> -->
+
+{@render children()}
